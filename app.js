69014261ec980a412a10a9005d84d36ade0f771f
Paweł <pawel.rzepinski@gmail.com>
2019-04-20 14:55:24 +0200

    first function

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
+const screen = document.querySelector(".calculator__screen");
 const btns = document.querySelectorAll(".btn");
+const numberBtn = document.querySelectorAll(".btn--number");
+const actionBtn = document.querySelectorAll(".btn--action");
+const multi = document.querySelector("#multiply");
+const division = document.querySelector("#division");
+const add = document.querySelector("#add");
+const substraction = document.querySelector("#substraction");
+const finalResult = document.querySelector("#result");
+let arr = [];
+let result = 0;
+
+const wyniki = function (){
+
+}
+
+
+const addAction = function (tab){
+    tab = arr;
+    tab.forEach(function(el){
+        result += el;        
+    })
+    return result;
+}
+
+addAction();
+
+
+const printResult = function () {
+    screen.innerText = result;
+}
+
+
+
+
+
+
+const addValue = function () {
+    console.log(this.innerText);
+
+    let number = parseInt(this.innerText, 10)
+    arr.push(number);
+    console.log(arr);
+    
+}
+
+
+
+numberBtn.forEach(function(el){
+    el.addEventListener("click", addValue);
+})
 
 btns.forEach(function(el){
     el.addEventListener("mousedown",function(){
@@ -12,3 +62,5 @@ btns.forEach(function(el){
     })
     
 })
+
+finalResult.addEventListener("click", printResult);
